d0d3180bd460deb3ad8d398e6b4916ce5f75d12f
Henrique Galli <gallihenrique28@gmail.com>
2021-11-05 20:27:02 -0300

    Manutenção iniciação do projeto

--- src/App.java
+++ src/App.java
@@ -1,15 +1,8 @@
-import com.sun.tools.javac.Main;
-
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 
 public class App extends Application {
     public static void main(String[] args) throws Exception {
